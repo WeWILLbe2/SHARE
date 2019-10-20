bb85d066f9fa2d414f9f6c8bb8b17bb820434b6f
MAY <2660397441@qq.com>
2019-10-20 13:20:49 +0800

    路由

--- share/myshare/src/views/Home.vue
+++ share/myshare/src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <div>
+  <el-menu-item index="1">
+    <em>SHARE</em>
+  </el-menu-item>
+  <el-menu-item index="2">
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+  </el-menu-item>
+  </div>
+  <div>
+  <el-menu-item index="3" >注册</el-menu-item>
+  <el-menu-item index="4">登录</el-menu-item>
+  <el-menu-item index="5">个人中心</el-menu-item>
+  </div>
+</el-menu>
+
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+<style  scoped>
+ul.el-menu-demo{
+  display:flex !important;
+  justify-content:space-between !important;
 }
+ul.el-menu-demo>div{
+   display:flex !important;
+   justify-content:space-evenly !important;
+}
+</style>
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1',
+        input:''
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
 </script>
